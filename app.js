c0e6a3c61bea51a401ebe6431329c70455f2f455
fenglan <fenglan.crazydance@gmail.com>
2015-02-28 18:53:51 +0800

    add static file

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ var port = process.env.PORT || 3000;
 var logger = require('koa-logger');
 var bodyparser = require('koa-bodyparser');
 var views = require('co-views');
-var render = views(__dirname + '/public');
+var render = views(__dirname + '/public', {ext: 'jade'});
 var flash = require('koa-flash');
 var session = require('koa-session');
+var router = require('koa-route');
+var serve = require('koa-static');
 
+app.use(serve(__dirname + '/public'));
 app.keys = ['Test koa session'];
 app.use(session(app));
 app.use(flash());
 app.use(bodyparser());
 app.use(logger());
 
-var router = require('koa-route');
-
-router.get('/', function *(){
-    this.body = yield render('index.html');
-});
+app.use(router.get('/', function *(){
+    this.body = yield render('index');
+}));
 
 app.listen(port);
 console.log('Listening on port' + port);
